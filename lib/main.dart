2a900891afbd9be75fa68e7e18cb7c0d10284c63
giovaneneves7 <autornekoyasha@gmail.com>
2025-02-09 19:15:22 -0300

    First Commit

--- lib/main.dart
+++ lib/main.dart
@@ -6,22 +6,16 @@ import 'package:corre_aqui/features/home/screens/home_screen.dart';
 import 'package:corre_aqui/features/splash/screens/post_splash_screen.dart';
 import 'package:corre_aqui/theme/custom_theme.dart';
 import 'package:corre_aqui/helper/route_helper.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-import 'firebase_options.dart';
 import 'helper/get_di.dart' as di;
 
 void main() async {
       
   WidgetsFlutterBinding.ensureInitialized();
-    await Firebase.initializeApp(
-      options: DefaultFirebaseOptions.currentPlatform,
-    );
-
-    await Supabase.initialize(
+  await Supabase.initialize(
       url: 'https://mpyyooswcudkgouiomuo.supabase.co',
       anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1weXlvb3N3Y3Vka2dvdWlvbXVvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzMwNTk5OTgsImV4cCI6MjA0ODYzNTk5OH0.zjQSRDm22Q8A-hCGyLLd9MObB9pIMCmf9bKg9aaE6AA',
-    );
+  );
 
   await di.init();
 
