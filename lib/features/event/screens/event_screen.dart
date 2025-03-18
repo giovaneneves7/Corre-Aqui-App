497f1f1abe1112c530465fdc948b757cc23bfa10
giovaneneves7 <autornekoyasha@gmail.com>
2025-03-18 15:30:48 -0300

    Update: Atualiza tela de evento

--- lib/features/event/screens/event_screen.dart
+++ lib/features/event/screens/event_screen.dart
@@ -1,6 +1,7 @@
 import 'package:corre_aqui/features/event/controllers/event_controller.dart';
 import 'package:corre_aqui/features/event/widgets/next_events_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 /**
 * @author Giovane Neves
@@ -15,31 +16,59 @@ class _EventScreenState extends State<EventScreen> {
    
   @override
   Widget build(BuildContext context) {
-     return Scaffold(
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                height: 180,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(8),
-                  image: DecorationImage(
-                    image: AssetImage('assets/images/event_highlight.jpg'),
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                alignment: Alignment.center,
+
+    return GetBuilder<EventController>(
+        builder: (eventController){
+          if (eventController.eventList.isEmpty) {
+            return Container(
+              height: 120,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                color: Colors.grey.shade300,
               ),
-              const SizedBox(height: 20),
-              NextEventsWidget(),
-            ],
-          ),
-        ),
-      ),
+              alignment: Alignment.center,
+              child: const Text(
+                "Carregando eventos...",
+                style: TextStyle(color: Colors.black),
+              ),
+            );
+          }
+
+          final featuredEvent = eventController.eventList.first;
+
+
+          return SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    height: 180,
+                    child: GestureDetector(
+                     // onTap: () => _handleRedirect(banner.redirectUrl),
+                      child: Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 10),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8),
+                          image: DecorationImage(
+                            image: NetworkImage(featuredEvent.imageUrl),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                    ),
+                    alignment: Alignment.center,
+                  ),
+                  const SizedBox(height: 20),
+                  NextEventsWidget(),
+                ],
+              ),
+            ),
+          );
+        }
     );
+
   }
 
 }
