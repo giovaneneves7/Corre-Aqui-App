cd60e18ecb8a58d0325f29351c82afd2799b739d
giovaneneves7 <autornekoyasha@gmail.com>
2025-03-21 10:08:37 -0300

    Feat(fonts): Adicionando fontes padrões

---
+++ lib/util/styles.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+// Estilos para LobsterTwo
+const lobsterTwoRegular = TextStyle(
+  fontFamily: 'LobsterTwo',
+  fontWeight: FontWeight.w400,
+);
+
+const lobsterTwoBold = TextStyle(
+  fontFamily: 'LobsterTwo',
+  fontWeight: FontWeight.w700,
+);
+
+const lobsterTwoItalic = TextStyle(
+  fontFamily: 'LobsterTwo',
+  fontStyle: FontStyle.italic,
+);
+
+const lobsterTwoBoldItalic = TextStyle(
+  fontFamily: 'LobsterTwo',
+  fontWeight: FontWeight.w700,
+  fontStyle: FontStyle.italic,
+);
+
+// Estilos para STIXTwoText
+const stixTwoTextRegular = TextStyle(
+  fontFamily: 'STIXTwoText',
+  fontWeight: FontWeight.w400,
+);
+
+const stixTwoTextBold = TextStyle(
+  fontFamily: 'STIXTwoText',
+  fontWeight: FontWeight.w700,
+);
+
+const stixTwoTextItalic = TextStyle(
+  fontFamily: 'STIXTwoText',
+  fontStyle: FontStyle.italic,
+);
